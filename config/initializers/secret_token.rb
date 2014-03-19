ac2e33d3f2d0a4ad6cb89d79eb884173abe9a121
Daniel Vu <evanidul@gmail.com>
2014-03-18 23:23:20 -0700

    let's get it started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smellyleaf::Application.config.secret_key_base = '49f46f70c434e71e989669c36e68c5d65053bc5f322e6bc2c24ddc64075cbccaaf15ac33300da3ed9287ca8f9fdc8d695e979052bd3bc3931d2fe0eba442d626'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smellyleaf::Application.config.session_store :cookie_store, key: '_smellyleaf_session'
