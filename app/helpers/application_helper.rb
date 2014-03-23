5b42c2a1d56751b540afb0be8379f888268097be
Daniel Vu <evanidul@gmail.com>
2014-03-23 03:14:27 -0700

    add registration modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+ 	#dvu: registration modal
+	def resource_name
+		:user
+	end
+
+	#dvu: registration modal
+	def resource
+		@resource ||= User.new
+	end
+
+	#dvu: registration modal
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
 end
