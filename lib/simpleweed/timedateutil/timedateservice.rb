d5323d6c07bc38faeff6fa7f5570746079f2e62a
Daniel Vu <evanidul@gmail.com>
2014-04-26 02:46:51 -0700

    nasty whitespace bug and string mutability bug : store hours storing 12AM as seconds correctly

--- lib/simpleweed/timedateutil/timedateservice.rb
+++ lib/simpleweed/timedateutil/timedateservice.rb
@@ -8,22 +8,30 @@ module Simpleweed
 
 	  		# rake test test/lib/simpleweed/timedateservice.rb
 	  		def getSecondsSinceMidnight(timestring)
-	  			# 10:00am - 11:30pm
+
 	  			if !timestring.is_a? String
 	  				return -1
 	  			end
+
+	  			#mutable strings lead to crazy bugs
+	  			timestring.freeze	  			
+
+				# .dup clones a string but unfreezes the cloned one
+	  			timestringcopy = timestring.dup
+
+	  			# 10:00am - 11:30pm
 	  			
-	  			if timestring.include? "pm"
+	  			if timestringcopy.include? "pm"
 	  				timestringIsPm = true
 	  			else 
 	  				timestringIsPm = false
 	  			end	
 
 	  			#delete am and pm
-	  			timestring.sub! 'am' , ''
-	  			timestring.sub! 'pm' , ''	  			
+	  			timestringcopy.sub! 'am' , ''
+	  			timestringcopy.sub! 'pm' , ''	  			
 
-	  			hoursAndMinutes = timestring.split(':')
+	  			hoursAndMinutes = timestringcopy.split(':')
 	  			#convert hours into number of seconds
 	  			hoursInSeconds = hoursAndMinutes[0].to_i * 60 * 60
 	  			minutesInSeconds = hoursAndMinutes[1].to_i * 60
@@ -31,13 +39,13 @@ module Simpleweed
 
 	  			if timestringIsPm
 	  				# if PM, add 12 hours to it.
-	  				if hoursAndMinutes[0] == "12"
+	  				if hoursAndMinutes[0].strip == "12" # call .strip because 10:00am - 12:00am leads to " 12"
 	  					return subtotal
 	  				else 
 	  					return subtotal + (12* 60 * 60) 
 	  				end
 	  			else 
-	  				if hoursAndMinutes[0] == "12"
+	  				if hoursAndMinutes[0].strip == "12"
 	  					return subtotal - (12 * 60 * 60)  # 12 am is midnight, so we should cancel out the 12 hours we added before.
 	  				else 
 	  					return subtotal;	
