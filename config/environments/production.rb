a6ab1220c2e90f6c345b2acde70d6fc6466d73f6
Daniel Vu <evanidul@gmail.com>
2014-05-01 20:37:33 -0700

    email verification: heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Smellyleaf::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # dvu: devise, email upon registration for verification
+  config.action_mailer.default_url_options = { :host => 'dry-ridge-1106.herokuapp.com' }
 end
