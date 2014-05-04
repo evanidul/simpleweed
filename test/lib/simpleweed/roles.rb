1e5dbae9a61598b43dd88dccf8262d9c7b4a8808
Daniel Vu <evanidul@gmail.com>
2014-05-03 21:55:00 -0700

    add tests to roles.rb

--- test/lib/simpleweed/roles.rb
+++ test/lib/simpleweed/roles.rb
@@ -34,6 +34,42 @@ class Roles < ActiveSupport::TestCase
 
 	end	
 
+	test "removing a scoped role doesn't affect other store instances" do
+		user = User.new(:email => 'test@example.com', :password => 'password', :password_confirmation => 'password')
+		user.save
 
+		# create Stores
+		store = Store.new
+		store.name = "ABC Weed"
+		store.save
+		store_other = Store.new
+		store_other.name = "CostCo Weed"
+		store_other.save
+
+		user.add_role :storeowner, store # sets a role for a resource instance
+		result = user.has_role? :storeowner, store
+		assert_equal( true, result, 'User should have storeowner role assigned but does not')
+
+		user.add_role :storeowner, store_other # sets a role for a resource instance
+		result_other_store = user.has_role? :storeowner, store_other
+		
+		#binding.pry
+		assert_equal( true, result_other_store, 'User should have storeowner role assigned but does not')
+
+		# delete role from first store
+		user.remove_role :storeowner, store
+		result_after_remove = user.has_role? :storeowner, store
+		assert_equal( false, result_after_remove, 'User should NOT have storeowner role assigned AFTER removal but does')
+
+		# other store should be the same		
+		other_result_after_remove = user.has_role? :storeowner, store_other
+		assert_equal( true, other_result_after_remove, 'User should have storeowner role assigned AFTER removal but does not')
+
+		# add it back just for yucks
+		user.add_role :storeowner, store # sets a role for a resource instance
+		result_after_adding_it_back = user.has_role? :storeowner, store
+		assert_equal( true, result_after_adding_it_back, 'User should have storeowner role assigned but does not')
+	end	
+	
 
 end
