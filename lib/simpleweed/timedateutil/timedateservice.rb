258323296260b34deea16710828821b368669801
Daniel Vu <evanidul@gmail.com>
2014-05-06 21:48:49 -0700

    comment out getSecondsSinceMidnight and tests (we've factored this logic out of the app)

--- lib/simpleweed/timedateutil/timedateservice.rb
+++ lib/simpleweed/timedateutil/timedateservice.rb
@@ -79,56 +79,56 @@ module Simpleweed
 
 	  		# @Tested: rake test test/lib/simpleweed/timedateservice.rb
 	  		# need to handle "Closed" case
-	  		def getSecondsSinceMidnight(timestring)
+	  	# 	def getSecondsSinceMidnight(timestring)
 
-	  			if !timestring.is_a? String
-	  				return -1
-	  			end
+	  	# 		if !timestring.is_a? String
+	  	# 			return -1
+	  	# 		end
 
-	  			if timestring == "Closed"
-	  				return -1
-	  			end
+	  	# 		if timestring == "Closed"
+	  	# 			return -1
+	  	# 		end
 
-	  			#mutable strings lead to crazy bugs
-	  			timestring.freeze	  			
+	  	# 		#mutable strings lead to crazy bugs
+	  	# 		timestring.freeze	  			
 
-				# .dup clones a string but unfreezes the cloned one
-	  			timestringcopy = timestring.dup
+				# # .dup clones a string but unfreezes the cloned one
+	  	# 		timestringcopy = timestring.dup
 
-	  			# 10:00am - 11:30pm
+	  	# 		# 10:00am - 11:30pm
 	  			
-	  			if timestringcopy.include? "pm"
-	  				timestringIsPm = true
-	  			else 
-	  				timestringIsPm = false
-	  			end	
+	  	# 		if timestringcopy.include? "pm"
+	  	# 			timestringIsPm = true
+	  	# 		else 
+	  	# 			timestringIsPm = false
+	  	# 		end	
 
-	  			#delete am and pm
-	  			timestringcopy.sub! 'am' , ''
-	  			timestringcopy.sub! 'pm' , ''	  			
+	  	# 		#delete am and pm
+	  	# 		timestringcopy.sub! 'am' , ''
+	  	# 		timestringcopy.sub! 'pm' , ''	  			
 
-	  			hoursAndMinutes = timestringcopy.split(':')
-	  			#convert hours into number of seconds
-	  			hoursInSeconds = hoursAndMinutes[0].to_i * 60 * 60
-	  			minutesInSeconds = hoursAndMinutes[1].to_i * 60
-	  			subtotal = hoursInSeconds + minutesInSeconds
+	  	# 		hoursAndMinutes = timestringcopy.split(':')
+	  	# 		#convert hours into number of seconds
+	  	# 		hoursInSeconds = hoursAndMinutes[0].to_i * 60 * 60
+	  	# 		minutesInSeconds = hoursAndMinutes[1].to_i * 60
+	  	# 		subtotal = hoursInSeconds + minutesInSeconds
 
-	  			if timestringIsPm
-	  				# if PM, add 12 hours to it.
-	  				if hoursAndMinutes[0].strip == "12" # call .strip because 10:00am - 12:00am leads to " 12"
-	  					return subtotal
-	  				else 
-	  					return subtotal + (12* 60 * 60) 
-	  				end
-	  			else 
-	  				if hoursAndMinutes[0].strip == "12"
-	  					return subtotal - (12 * 60 * 60)  # 12 am is midnight, so we should cancel out the 12 hours we added before.
-	  				else 
-	  					return subtotal;	
-	  				end
-	  			end
+	  	# 		if timestringIsPm
+	  	# 			# if PM, add 12 hours to it.
+	  	# 			if hoursAndMinutes[0].strip == "12" # call .strip because 10:00am - 12:00am leads to " 12"
+	  	# 				return subtotal
+	  	# 			else 
+	  	# 				return subtotal + (12* 60 * 60) 
+	  	# 			end
+	  	# 		else 
+	  	# 			if hoursAndMinutes[0].strip == "12"
+	  	# 				return subtotal - (12 * 60 * 60)  # 12 am is midnight, so we should cancel out the 12 hours we added before.
+	  	# 			else 
+	  	# 				return subtotal;	
+	  	# 			end
+	  	# 		end
 
-	  		end #getSecondsSinceMidnight
+	  	# 	end #getSecondsSinceMidnight
 
 	  		# 0-23 for hours, 0-59 for minutes
 	  		def getSecondsSinceMidnightGivenHourAndMinute(hour, minute)
--- test/lib/simpleweed/timedateservice.rb
+++ test/lib/simpleweed/timedateservice.rb
@@ -2,92 +2,92 @@ require 'test_helper'
  
 class Timedateservice < ActiveSupport::TestCase
 
-	test "getSecondsSinceMidnight 1am" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "1:00 am"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 3600, result, 'Expected 3600 to be result')
-	end
+	# test "getSecondsSinceMidnight 1am" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "1:00 am"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 3600, result, 'Expected 3600 to be result')
+	# end
 
-	test "getSecondsSinceMidnight non-string returns -1" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = 12
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( -1, result, 'non-strings should return -1 but didnt')
-	end
+	# test "getSecondsSinceMidnight non-string returns -1" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = 12
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( -1, result, 'non-strings should return -1 but didnt')
+	# end
 
-	test "getSecondsSinceMidnight 8:30am" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "8:30 am"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 30600, result, 'Expected 30600 to be result')
-	end
+	# test "getSecondsSinceMidnight 8:30am" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "8:30 am"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 30600, result, 'Expected 30600 to be result')
+	# end
 
-	test "getSecondsSinceMidnight 1:00 pm" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "1:00 pm"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 46800, result, 'Expected 46800 to be result')
-	end
+	# test "getSecondsSinceMidnight 1:00 pm" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "1:00 pm"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 46800, result, 'Expected 46800 to be result')
+	# end
 
-	test "getSecondsSinceMidnight 10:45 pm" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "10:45 pm"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 81900, result, 'Expected 81900 to be result')
-	end
+	# test "getSecondsSinceMidnight 10:45 pm" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "10:45 pm"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 81900, result, 'Expected 81900 to be result')
+	# end
 
-	test "getSecondsSinceMidnight 12:00 am (midnight)" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "12:00 am"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 0, result, 'Expected 0 to be result')
-	end
+	# test "getSecondsSinceMidnight 12:00 am (midnight)" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "12:00 am"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 0, result, 'Expected 0 to be result')
+	# end
 
-	test "getSecondsSinceMidnight 12:15 am (midnight)" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "12:15 am"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 900, result, 'Expected 900 to be result')
-	end
+	# test "getSecondsSinceMidnight 12:15 am (midnight)" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "12:15 am"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 900, result, 'Expected 900 to be result')
+	# end
 
-	test "getSecondsSinceMidnight 12:00 pm (noon)" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "12:00 pm"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 12 * 60 * 60, result,'Expected 43,200 to be result')
-	end
+	# test "getSecondsSinceMidnight 12:00 pm (noon)" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "12:00 pm"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 12 * 60 * 60, result,'Expected 43,200 to be result')
+	# end
 
-	test "getSecondsSinceMidnight 12:35 pm (noon)" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "12:35 pm"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 45300, result, 'Expected 45,300 to be result')
-	end
+	# test "getSecondsSinceMidnight 12:35 pm (noon)" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "12:35 pm"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 45300, result, 'Expected 45,300 to be result')
+	# end
 
-	test "getSecondsSinceMidnight from actual parsed string" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = "6:00am - 12:00am"
-		split =  testString.split("-")
-		openOrig = split[0]
-		closedOrig = split[1]
+	# test "getSecondsSinceMidnight from actual parsed string" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = "6:00am - 12:00am"
+	# 	split =  testString.split("-")
+	# 	openOrig = split[0]
+	# 	closedOrig = split[1]
 
-		assert_equal( " 12:00am", closedOrig, "Expected 12:00am as string")
+	# 	assert_equal( " 12:00am", closedOrig, "Expected 12:00am as string")
 
-		open = service.getSecondsSinceMidnight(openOrig)
-		closed = service.getSecondsSinceMidnight(closedOrig)     		
+	# 	open = service.getSecondsSinceMidnight(openOrig)
+	# 	closed = service.getSecondsSinceMidnight(closedOrig)     		
 		
-		assert_equal( " 12:00am", closedOrig, "Expected 12:00am as string")
-		assert_equal( 21600, open, 'Expected 21600 to be result')
-		assert_equal( 0, closed, 'Expected 0 to be result')
-	end
+	# 	assert_equal( " 12:00am", closedOrig, "Expected 12:00am as string")
+	# 	assert_equal( 21600, open, 'Expected 21600 to be result')
+	# 	assert_equal( 0, closed, 'Expected 0 to be result')
+	# end
 
-	test "getSecondsSinceMidnight 12:00 am (midnight), with space in front" do
-		service = Simpleweed::Timedateutil::Timedateservice.new
-		testString = " 12:00 am"
-		result = service.getSecondsSinceMidnight(testString)
-		assert_equal( 0, result, 'Expected 0 to be result')
-	end
+	# test "getSecondsSinceMidnight 12:00 am (midnight), with space in front" do
+	# 	service = Simpleweed::Timedateutil::Timedateservice.new
+	# 	testString = " 12:00 am"
+	# 	result = service.getSecondsSinceMidnight(testString)
+	# 	assert_equal( 0, result, 'Expected 0 to be result')
+	# end
 	
 	##
 	## doesTimeOccurDuringBusinessHours(open, closed, current, previousDayOpen, previousDayClose)
