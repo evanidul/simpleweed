6a469492bff629ccf48fc58e3e036cd8983d78d7
Daniel Vu <evanidul@gmail.com>
2014-05-07 20:13:16 -0700

    remove basic auth temporarily for google verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  http_basic_authenticate_with name: "ddadmin", password: "idontreallysmoke"
+  #http_basic_authenticate_with name: "ddadmin", password: "idontreallysmoke"
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
