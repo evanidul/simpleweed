0add67b8ca0a6d73a9170aa1a16635ebf6936e82
Daniel Vu <evanidul@gmail.com>
2014-05-18 09:57:43 -0700

    bug fix for tab IDs, toggle coverage via env param

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
-require 'simplecov'
-SimpleCov.start
+
+# dvu: add 2 lines to get coverage report.  Wondering if it's causing selenium delays, so commenting out.
+if ENV['COVERAGE'] == "true"
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
