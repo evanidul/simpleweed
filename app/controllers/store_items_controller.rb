653038001ac447ef8f020b76dd7f6fd16ddb779d
Daniel Vu <evanidul@gmail.com>
2014-05-18 22:43:02 -0700

    add main category to items

--- app/controllers/store_items_controller.rb
+++ app/controllers/store_items_controller.rb
@@ -47,7 +47,7 @@ private
 private 
 	def store_item_params
 		params.require(:store_item).permit(:name, :store_id, :description, :thc, :cbd, :cbn, :costhalfgram, :costonegram, :costeighthoz, :costquarteroz,
-			:costhalfoz, :costoneoz, :costperunit, :dogo, :strain)		
+			:costhalfoz, :costoneoz, :costperunit, :dogo, :strain, :maincategory)		
 	end		
 
 end
--- app/models/store_item.rb
+++ app/models/store_item.rb
@@ -1,8 +1,13 @@
 class StoreItem < ActiveRecord::Base
-  belongs_to :store
-  validates :name, presence: true
+	belongs_to :store
+	validates :name, presence: true
 
-  STRAINS = ['','indica', 'sativa', 'hybrid']
-  validates_inclusion_of :strain, :in => STRAINS,
-          :message => "{{value}} must be in #{STRAINS.join ','}"
+	STRAINS = ['','indica', 'sativa', 'hybrid']
+	validates_inclusion_of :strain, :in => STRAINS,
+	      :message => "{{value}} must be in #{STRAINS.join ','}"
+
+	CATEGORIES = ['flower', 'concentrate', 'edible', 'pre-roll', 'other', 'accessory']
+	validates_inclusion_of :maincategory, :in => CATEGORIES,
+	      :message => "{{value}} must be in #{CATEGORIES.join ','}"
+
 end
