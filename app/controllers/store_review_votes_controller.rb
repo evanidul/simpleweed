7cb9148135a7d60d03dd86a9c91cd1a7bd217c0f
Daniel Vu <evanidul@gmail.com>
2014-06-04 06:40:29 -0700

    wire ajax votes to reviews

--- app/controllers/store_review_votes_controller.rb
+++ app/controllers/store_review_votes_controller.rb
@@ -1,2 +1,38 @@
 class StoreReviewVotesController < ApplicationController
+
+before_filter :load_store_review
+
+	def create		
+		@store_review_vote =  @storereview.store_review_votes.create(store_review_votes_params)		
+		@store_review_vote.user = current_user
+
+		respond_to do |format|
+      		#format.html # new.html.erb
+      		#format.json { render json: @store_review_vote }
+      		format.js
+
+      		@vote = store_review_votes_params[:vote];
+      		
+
+
+			if @store_review_vote.save
+
+				@currentcount = @storereview.store_review_votes.sum(:vote)
+				format.js {}
+			else 
+				return
+			end
+		end # respond_to
+	end
+
+private
+    def load_store_review
+      @storereview = StoreReview.find(params[:store_review_id])
+    end
+
+private 
+	def store_review_votes_params
+		params.require(:store_review_vote).permit(:store_review_id, :user_id, :vote)		
+	end		
+
 end
