b81968048214572ae20089a6d66c0118563e4e44
Daniel Vu <evanidul@gmail.com>
2014-06-12 02:29:29 -0700

    disable indexing for import

--- app/models/store_item.rb
+++ app/models/store_item.rb
@@ -31,127 +31,129 @@ class StoreItem < ActiveRecord::Base
 		return Sunspot::Util::Coordinates.new(store.latitude, store.longitude)
 	end
 
-  	searchable do
-  		integer  :id, :stored => true
-    	text     :name, :stored => true
-	   	text     :description, :stored => true
-	    float    :thc
-	    float    :cbd
-	    float    :cbn
-	    integer  :costhalfgram, :stored => true
-	    integer  :costonegram, :stored => true
-	    integer  :costeighthoz, :stored => true
-	    integer  :costquarteroz, :stored => true
-	    integer  :costhalfoz, :stored => true
-	    integer  :costoneoz, :stored => true
-	    boolean  :dogo
-	    #datetime :created_at
-	    #datetime :updated_at
-	    string   :category, :stored => true
-	    integer  :costperunit
-	    string   :maincategory
-	    string   :strain
-	    string   :subcategory
-	    string   :cultivation
-	    boolean  :privatereserve
-	    boolean  :topshelf
-	    boolean  :supersize
-	    boolean  :glutenfree
-	    boolean  :sugarfree
-	    boolean  :organic
 
-	    boolean  :og
-	    boolean  :kush
-	    boolean  :haze
+	# DVU: turn off this block to prevent indexing for large data loads.  Turn back on after import is complete
+  # 	searchable do
+  # 		integer  :id, :stored => true
+  #   	text     :name, :stored => true
+	 #   	text     :description, :stored => true
+	 #    float    :thc
+	 #    float    :cbd
+	 #    float    :cbn
+	 #    integer  :costhalfgram, :stored => true
+	 #    integer  :costonegram, :stored => true
+	 #    integer  :costeighthoz, :stored => true
+	 #    integer  :costquarteroz, :stored => true
+	 #    integer  :costhalfoz, :stored => true
+	 #    integer  :costoneoz, :stored => true
+	 #    boolean  :dogo
+	 #    #datetime :created_at
+	 #    #datetime :updated_at
+	 #    string   :category, :stored => true
+	 #    integer  :costperunit
+	 #    string   :maincategory
+	 #    string   :strain
+	 #    string   :subcategory
+	 #    string   :cultivation
+	 #    boolean  :privatereserve
+	 #    boolean  :topshelf
+	 #    boolean  :supersize
+	 #    boolean  :glutenfree
+	 #    boolean  :sugarfree
+	 #    boolean  :organic
 
-	    integer :store_id, :stored => true
-	    string(:store_id_str) { |p| p.store.id.to_s }
+	 #    boolean  :og
+	 #    boolean  :kush
+	 #    boolean  :haze
+
+	 #    integer :store_id, :stored => true
+	 #    string(:store_id_str) { |p| p.store.id.to_s }
 	    
-	    # store info	   
-	    # NOTE, store id is fetched above
+	 #    # store info	   
+	 #    # NOTE, store id is fetched above
 
-	    text :store_name , :stored => true do
-		   store.name 
-	  	end
+	 #    text :store_name , :stored => true do
+		#    store.name 
+	 #  	end
 
-	    text :store_addressline1 , :stored => true do
-		   store.addressline1
-	  	end
+	 #    text :store_addressline1 , :stored => true do
+		#    store.addressline1
+	 #  	end
 
-	    text :store_addressline2 , :stored => true do
-		   store.addressline2
-	  	end
+	 #    text :store_addressline2 , :stored => true do
+		#    store.addressline2
+	 #  	end
 
-	    text :store_city , :stored => true do
-		   store.city
-	  	end
+	 #    text :store_city , :stored => true do
+		#    store.city
+	 #  	end
 	  	
-		text :store_state , :stored => true do
-		   store.state
-	  	end	  	
-	  	text :store_zip , :stored => true do
-		   store.zip
-	  	end
+		# text :store_state , :stored => true do
+		#    store.state
+	 #  	end	  	
+	 #  	text :store_zip , :stored => true do
+		#    store.zip
+	 #  	end
 
-	  	text :store_phonenumber , :stored => true do
-		   store.phonenumber
-	  	end
+	 #  	text :store_phonenumber , :stored => true do
+		#    store.phonenumber
+	 #  	end
 	  	
-	  	latlon :location, :stored => true
-	  	float :store_latitude, :stored => true do
-	  		store.latitude
-	  	end
-	  	float :store_longitude, :stored => true do
-	  		store.longitude
-	  	end
+	 #  	latlon :location, :stored => true
+	 #  	float :store_latitude, :stored => true do
+	 #  		store.latitude
+	 #  	end
+	 #  	float :store_longitude, :stored => true do
+	 #  		store.longitude
+	 #  	end
 
-	  	# store features
-	  	boolean :store_deliveryservice, :stored => true do
-	  		store.deliveryservice
-	  	end
-	  	boolean :store_acceptscreditcards, :stored => true do
-	  		store.acceptscreditcards
-	  	end
-	  	boolean :store_atmaccess, :stored => true do
-	  		store.atmaccess
-	  	end
-	  	boolean :store_automaticdispensingmachines, :stored => true do
-	  		store.automaticdispensingmachines
-	  	end
+	 #  	# store features
+	 #  	boolean :store_deliveryservice, :stored => true do
+	 #  		store.deliveryservice
+	 #  	end
+	 #  	boolean :store_acceptscreditcards, :stored => true do
+	 #  		store.acceptscreditcards
+	 #  	end
+	 #  	boolean :store_atmaccess, :stored => true do
+	 #  		store.atmaccess
+	 #  	end
+	 #  	boolean :store_automaticdispensingmachines, :stored => true do
+	 #  		store.automaticdispensingmachines
+	 #  	end
 
-	  	boolean :store_firsttimepatientdeals, :stored => true do
-	  		store.firsttimepatientdeals
-	  	end
-	  	boolean :store_handicapaccess, :stored => true do
-	  		store.handicapaccess
-	  	end
-	  	boolean :store_loungearea, :stored => true do
-	  		store.loungearea
-	  	end
-	  	boolean :store_petfriendly, :stored => true do
-	  		store.petfriendly
-	  	end
+	 #  	boolean :store_firsttimepatientdeals, :stored => true do
+	 #  		store.firsttimepatientdeals
+	 #  	end
+	 #  	boolean :store_handicapaccess, :stored => true do
+	 #  		store.handicapaccess
+	 #  	end
+	 #  	boolean :store_loungearea, :stored => true do
+	 #  		store.loungearea
+	 #  	end
+	 #  	boolean :store_petfriendly, :stored => true do
+	 #  		store.petfriendly
+	 #  	end
 
-	  	boolean :store_securityguard, :stored => true do
-	  		store.securityguard
-	  	end
-	  	boolean :store_eighteenplus, :stored => true do
-	  		store.eighteenplus
-	  	end
-	  	boolean :store_twentyoneplus, :stored => true do
-	  		store.twentyoneplus
-	  	end
-	  	boolean :store_hasphotos, :stored => true do
-	  		store.hasphotos
-	  	end
-	  	boolean :store_labtested, :stored => true do
-	  		store.labtested
-	  	end
-	  	boolean :store_onsitetesting, :stored => true do
-	  		store.onsitetesting
-	  	end
+	 #  	boolean :store_securityguard, :stored => true do
+	 #  		store.securityguard
+	 #  	end
+	 #  	boolean :store_eighteenplus, :stored => true do
+	 #  		store.eighteenplus
+	 #  	end
+	 #  	boolean :store_twentyoneplus, :stored => true do
+	 #  		store.twentyoneplus
+	 #  	end
+	 #  	boolean :store_hasphotos, :stored => true do
+	 #  		store.hasphotos
+	 #  	end
+	 #  	boolean :store_labtested, :stored => true do
+	 #  		store.labtested
+	 #  	end
+	 #  	boolean :store_onsitetesting, :stored => true do
+	 #  		store.onsitetesting
+	 #  	end
 
-    end
-
+  #   end  
+    # searchable do
 
 end
