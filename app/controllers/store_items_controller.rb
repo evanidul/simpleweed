a85b0e195812a296962c5ae4471e39978694b3b2
Daniel Vu <evanidul@gmail.com>
2014-06-20 20:09:18 -0700

    hide store tab for item popups on store page

--- app/controllers/store_items_controller.rb
+++ app/controllers/store_items_controller.rb
@@ -34,6 +34,7 @@ class StoreItemsController < ApplicationController
 
 		@item_reviews = @store_item.store_item_reviews
 		
+		@storetab = params[:storetab]		
 		render layout: false
 	end
 
@@ -61,7 +62,7 @@ private
 	def store_item_params
 		params.require(:store_item).permit(:name, :store_id, :description, :thc, :cbd, :cbn, :costhalfgram, :costonegram, :costeighthoz, :costquarteroz,
 			:costhalfoz, :costoneoz, :costperunit, :dogo, :strain, :maincategory, :subcategory, :cultivation,
-			:privatereserve, :topshelf, :supersize, :glutenfree, :sugarfree, :organic, :dose, :og, :kush, :haze)		
+			:privatereserve, :topshelf, :supersize, :glutenfree, :sugarfree, :organic, :dose, :og, :kush, :haze, :storetab)		
 	end		
 
 end
