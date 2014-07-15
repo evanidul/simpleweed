437ca23bcbe0babe9e64d742d7b219e8d8c71f99
Daniel Vu <evanidul@gmail.com>
2014-07-15 03:00:44 -0700

    community home page now populated from other feeds

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,7 +2,22 @@
 class FeedsController < ApplicationController
 
 	def index
+		# @feeds used for top feed nav
 		@feeds = Feed.all
+				
+		@top_posts_from_today = []
+		# TODO: fill this array with top posts from each feed for the current day
+		@feeds.each do | feed | 
+			# Post.where("created_at >= ?", Time.zone.now.beginning_of_day)
+			todays_posts = feed.feed_posts.where("created_at >= ?", Time.zone.now.beginning_of_day)
+			todays_most_popular_posts = todays_posts.sort_by {|post| post.sum_votes}.reverse
+			for i in 0..3
+				if todays_most_popular_posts[i]
+					@top_posts_from_today << todays_most_popular_posts[i]
+				end
+			end
+
+		end
 	end
 
 	def new
@@ -16,7 +31,10 @@ class FeedsController < ApplicationController
 	end
 
 	def show
+		# @feeds used for top feed nav
 		@feeds = Feed.all
+
+		# @feed : render the current feed
 		@feed = Feed.find(params[:id])
 	end 
 
