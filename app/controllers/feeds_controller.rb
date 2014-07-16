c00568ae1304374c47e64cb43d2066eebcdab55a
Daniel Vu <evanidul@gmail.com>
2014-07-15 21:13:53 -0700

    if no new posts today, scan the whole week

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,9 +8,8 @@ class FeedsController < ApplicationController
 		@roleservice = Simpleweed::Security::Roleservice.new
 				
 		@top_posts_from_today = []
-		# TODO: fill this array with top posts from each feed for the current day
-		@feeds.each do | feed | 
-			# Post.where("created_at >= ?", Time.zone.now.beginning_of_day)
+		
+		@feeds.each do | feed | 		
 			todays_posts = feed.feed_posts.where("created_at >= ?", Time.zone.now.beginning_of_day)
 			todays_most_popular_posts = todays_posts.sort_by {|post| post.sum_votes}.reverse
 			for i in 0..3
@@ -18,8 +17,28 @@ class FeedsController < ApplicationController
 					@top_posts_from_today << todays_most_popular_posts[i]
 				end
 			end
-
 		end
+
+		if @top_posts_from_today.size > 20
+			@top_posts = @top_posts_from_today
+			return
+		end
+
+		@top_posts_from_this_week = []
+
+		@feeds.each do | feed | 		
+			this_weeks_posts = feed.feed_posts.where("created_at >= ?", Time.zone.now.beginning_of_week)
+			this_weeks_most_popular_posts = this_weeks_posts.sort_by {|post| post.sum_votes}.reverse
+			for i in 0..3
+				if this_weeks_most_popular_posts[i]
+					@top_posts_from_this_week << this_weeks_most_popular_posts[i]
+				end
+			end
+		end
+
+		@top_posts = @top_posts_from_this_week
+
+
 	end
 
 	def new
