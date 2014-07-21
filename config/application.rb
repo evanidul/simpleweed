59895bb6e6d3ff6e5d16395a923aba2ff2b30500
Daniel Vu <evanidul@gmail.com>
2014-07-20 20:25:40 -0700

    fix email paths

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Smellyleaf
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/lib)
+    config.action_mailer.default_url_options = { host: 'simpleweed.org' }
   end
 end
