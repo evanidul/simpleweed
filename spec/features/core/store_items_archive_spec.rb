c4a370db2a099a3d740df84ee535e19778c2cdf2
Daniel Vu <evanidul@gmail.com>
2014-08-20 04:47:47 -0700

    test: archiving an item archives reviews as well and doesn't break profile page nor comm recent reviews page

--- spec/features/core/store_items_archive_spec.rb
+++ spec/features/core/store_items_archive_spec.rb
@@ -11,6 +11,11 @@ require 'pages/store_search_preview.rb'
 require 'pages/store_items_archive_prompt.rb'
 require 'pages/store_archived_items.rb'
 require 'pages/search_results_items'
+require 'pages/itempopup'
+require 'page_components/profile_nav'
+require 'pages/profile_myreviews'
+require 'page_components/community_nav'
+require 'pages/community_recent_item_reviews'
 
 feature "store item archive tests" , :js => true, :search =>true do
 
@@ -78,12 +83,7 @@ feature "store item archive tests" , :js => true, :search =>true do
 
 		items_page.save_store_item_button.click
 
-		# back to items index
-		row_links = items_page.row_links
-
-	    	#items_page.searchresults[index].click
-	    	#items_page.searchresults.first.click # most recently added will be on top?
-	    	 
+		# back to items index			
 	    	items_page.searchresults.each {|item_link| 
 	    		
 	    		if item_link.text.include? item_name
@@ -194,6 +194,144 @@ feature "store item archive tests" , :js => true, :search =>true do
 		store_page = StorePage.new
 		expect(page).to have_text(@item1name)
 		expect(page).should_not have_text(item_name)
+	end
+
+	scenario "create a store, add some items, review item, archive item, profile feed should not show review" do
+		page.visit("/users/sign_in")
+		login_page = LoginPage.new
+		login_page.username_input.set @adminemail
+    	login_page.username_password_input.set @adminpassword
+    	login_page.sign_in_button.click
+
+    	page.visit(store_path(@store))
+    	store_page = StorePage.new
+
+    	page.visit(store_store_items_path(@store))
+    	items_page = StoreItemsPage.new		
+		expect(items_page.store_name.text).to have_text(@store_name)
+		items_page.add_store_item_button.click
+
+		# add new item
+		item_name = "Weedy"
+		items_page.store_item_name.set item_name
+		items_page.store_item_strain.select 'sativa'
+		items_page.store_item_maincategory.select 'flower'
+		items_page.store_item_subcategory.select 'bud'
+
+		items_page.save_store_item_button.click
+
+		# search and write a review		
+        header = HeaderPageComponent.new    
+        header.search_input.set "7110 Rock Valley Court, San Diego, CA"
+        header.item_query_input.set item_name
+        header.search_button.click
+
+        search_results_page = SearchResultsItemPageComponent.new        
+                
+        search_results_page.searchresults_item_names.size.should == 1
+        search_results_page.searchresults_item_names.map {|name| name.text}.should == [item_name]
+
+        # click and view preview        
+        search_results_page.searchresults_item_names.first.click
+        
+        itempopup = ItemPopupComponent.new
+        wait_for_ajax
+        assert_modal_visible
+        itempopup.tab_reviews.click
+
+        itempopup.write_review_button.click
+        review_text = "i loved this thing"
+        itempopup.review_text_input.set review_text
+        itempopup.save_review_button.click
+        itempopup.cancel_button.click    
+        
+        # go to your profile        
+        page.visit("/")
+        header.edituserlink.click           
+        profile_nav = ProfileNavPageComponent.new
+        profile_nav.my_reviews_link.click
+
+        myreviews_page = ProfileMyReviewsPageComponent.new
+        myreviews_page.item_review_tab.click
+        myreviews_page.item_reviews.size.should == 1      
+
+        # go to recent item reviews in comm, it should be there        
+        header.community_home_link.click
+        community_nav = CommunityNavPageComponent.new
+        community_nav.recent_item_reviews_link.click
+        community_recent_item_reviews_page = CommunityRecentItemReviewsPageComponent.new
+        community_recent_item_reviews_page.recent_item_reviews.size.should == 1
+        
+        # archive the item
+        page.visit(store_store_items_path(@store))
+
+        items_page.searchresults.each {|item_link| 
+	    		
+	    		if item_link.text.include? item_name
+	    			item_link.click
+	    			break
+	    		end
+
+	    	}
+
+	    items_page.archive_item_button.click
+
+	    store_items_archive_prompt_page = StoreItemsArchivePromptPage.new
+		store_items_archive_prompt_page.archive_button.click
+
+		expect(page).to have_text('has been archived')
+
+		# go to your profile        
+        page.visit("/")
+        header.edituserlink.click           
+        profile_nav = ProfileNavPageComponent.new
+        profile_nav.my_reviews_link.click
+
+        myreviews_page = ProfileMyReviewsPageComponent.new
+        myreviews_page.item_review_tab.click
+        # the review for the archived item should be soft deleted as well
+        myreviews_page.item_reviews.size.should == 0
+
+        # go to recent item reviews in comm, it should NOT be there        
+        header.community_home_link.click
+        community_nav = CommunityNavPageComponent.new
+        community_nav.recent_item_reviews_link.click
+        community_recent_item_reviews_page = CommunityRecentItemReviewsPageComponent.new
+        community_recent_item_reviews_page.recent_item_reviews.size.should == 0
+
+        # now unarchive it
+        page.visit(store_store_items_path(@store))
+		items_page.archived_items_button.click
+		
+		archived_items_page = StoreArchivedItemsPage.new
+    	archived_items_page.searchresults.each {|item_link| 
+    		
+    		if item_link.text.include? item_name
+    			item_link.click
+    			break
+    		end
+
+    	}
+
+    	archived_items_page.unarchive_button.click
+
+		expect(page).to have_text('has been unarchived')
+
+		# reviews should be back in profile		
+		header.edituserlink.click           
+		profile_nav.my_reviews_link.click
+
+        myreviews_page = ProfileMyReviewsPageComponent.new
+        myreviews_page.item_review_tab.click
+        # the review for the archived item should be soft deleted as well
+        myreviews_page.item_reviews.size.should == 1
+
+        # go to recent item reviews in comm, it should be there again
+        header.community_home_link.click
+        community_nav = CommunityNavPageComponent.new
+        community_nav.recent_item_reviews_link.click
+        community_recent_item_reviews_page = CommunityRecentItemReviewsPageComponent.new
+        community_recent_item_reviews_page.recent_item_reviews.size.should == 1
 	end
 
 end	
