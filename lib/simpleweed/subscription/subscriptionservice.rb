800f18136c970ded330f8228a9683faa9096aa7d
Daniel Vu <evanidul@gmail.com>
2014-09-04 19:23:12 -0700

    subscription service tests, map feature names to plan IDs

--- lib/simpleweed/subscription/subscriptionservice.rb
+++ lib/simpleweed/subscription/subscriptionservice.rb
@@ -4,7 +4,24 @@ module Simpleweed
 		class Subscriptionservice
 
 			@@plan_name = ['','gram','quarter ounce', 'ounce']
-			@@plan_cost_per_month = ['','9','99','299']
+			@@plan_cost_per_month = ['','19','99','299']
+
+			@@feature_to_plan_hash = Hash.new
+			@@feature_to_plan_hash["store-address-contact-hours"] = 1
+			@@feature_to_plan_hash["ratings-reviews"] = 1
+			@@feature_to_plan_hash["member-alerts"] = 1
+			@@feature_to_plan_hash["store-menu"] = 1
+
+			@@feature_to_plan_hash["store-promo"] = 2
+			@@feature_to_plan_hash["store-description"] = 2
+			@@feature_to_plan_hash["store-features"] = 2
+			@@feature_to_plan_hash["store-image-hosting"] = 2
+
+			@@feature_to_plan_hash["store-daily-specials"] = 3
+			@@feature_to_plan_hash["store-first-time-patient-deals"] = 3
+			@@feature_to_plan_hash["item-expanded-view"] = 3
+			@@feature_to_plan_hash["item-promotions"] = 3			
+
 
 			def getPlanNameFromID(id)				
 				return @@plan_name[id]
@@ -12,6 +29,28 @@ module Simpleweed
 
 			def getPlanCostPerMonthFromID(id)
 				return @@plan_cost_per_month[id]
+			end
+
+			def getPlanForFeature(featureName)
+				return @@feature_to_plan_hash[featureName]
+			end
+
+			def canStoreUseFeature(store, featureName)
+				if store.blank?
+					return false
+				end
+
+				if store.plan_id.blank?
+					return false
+				end
+
+				storeplanSufficient = store.plan_id >= getPlanForFeature(featureName)
+				
+				if storeplanSufficient
+					return true
+				else
+					return false
+				end
 			end
 
 		end #class
---
+++ test/lib/simpleweed/subscriptiontests.rb
@@ -0,0 +1,45 @@
+require 'test_helper'
+ 
+# Tests our integraiton with https://github.com/medihack/make_flaggable at a model level.  Does not test at a view level.
+
+class SubscriptionTests < ActiveSupport::TestCase
+
+	test "basic subscription test" do		
+
+		store1 = Store.new(:name => 'some store')
+		test_plan_id = 1
+		store1.plan_id = test_plan_id
+		store1.save
+		assert_equal(Store.count, 1)
+
+		@subscriptionservice = Simpleweed::Subscription::Subscriptionservice.new
+
+		test_plan_name = @subscriptionservice.getPlanNameFromID(test_plan_id)
+				
+		assert_equal( 'gram', test_plan_name, 'mismatched plan')
+
+		test_plan_cost = @subscriptionservice.getPlanCostPerMonthFromID(test_plan_id)
+
+		assert_equal( '19', test_plan_cost, 'mismatched plan cost')		
+
+		test_plan_for_feature = @subscriptionservice.getPlanForFeature("store-address-contact-hours")
+
+		assert_equal( 1, test_plan_for_feature, 'mismatched plan to feature')				
+
+		test_plan_for_feature2 = @subscriptionservice.getPlanForFeature("store-daily-specials")		
+
+		assert_equal( 3, test_plan_for_feature2, 'mismatched plan to feature')
+
+		test_can_use_address_feature = @subscriptionservice.canStoreUseFeature(store1,"member-alerts")
+
+		assert_equal( true, test_can_use_address_feature, 'store should be able to use this feature')
+
+		test_can_use_address_feature2 = @subscriptionservice.canStoreUseFeature(store1,"store-promo")		
+
+		assert_equal( false, test_can_use_address_feature2, 'store should not be able to use this feature' )
+
+	end
+
+
+
+end
