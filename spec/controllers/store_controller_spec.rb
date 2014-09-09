95df793c072c214defa86df1ebaa4ff09d422d58
Daniel Vu <evanidul@gmail.com>
2014-09-08 21:51:09 -0700

    controller test cases

--- spec/controllers/store_controller_spec.rb
+++ spec/controllers/store_controller_spec.rb
@@ -26,6 +26,30 @@ describe StoresController do
 	end
 
 	describe 'update_promo' do
+		it "WORKING: update a store's promo works if admin user is logged in and plan_id is 2 or greater" do						
+			@store.plan_id = 3			
+			@store.save															
+			sign_in @admin			
+			@new_promo = 'first store promo'
+    		put :update_promo, id: @store.id, store: {promo: @new_promo}
+    		expect(response).to redirect_to store_url
+    		@store.reload
+    		expect(@store.promo).to eq(@new_promo)
+		end
+
+		it "WORKING: update a store's promo works if the user is a store manager and plan_id is 2 or greater" do
+			@store.plan_id = 3			
+			@store.save								
+			role_service = Simpleweed::Security::Roleservice.new							
+			role_service.addStoreOwnerRoleToStore(@user, @store)
+			sign_in @user
+			@new_promo = 'first store promo'
+    		put :update_promo, id: @store.id, store: {promo: @new_promo}
+    		expect(response).to redirect_to store_url
+    		@store.reload
+    		expect(@store.promo).to eq(@new_promo)
+		end
+
 		it "update a store's promo requires login" do
 			#sign_in user
     		put :update_promo, id: @store.id, store: {promo: 'first store promo'}
@@ -40,17 +64,6 @@ describe StoresController do
     		expect(@store.promo).to be_nil     		
 		end
 
-		it "update a store's promo works if admin user is logged in and plan_id is 2 or greater" do						
-			@store.plan_id = 3			
-			@store.save															
-			sign_in @admin			
-			@new_promo = 'first store promo'
-    		put :update_promo, id: @store.id, store: {promo: @new_promo}
-    		expect(response).to redirect_to store_url
-    		@store.reload
-    		expect(@store.promo).to eq(@new_promo)
-		end
-
 		it "update a store's promo redirects to subscription page if admin user is logged in but plan_id is 1 " do						
 			@store.plan_id = 1
 			@store.save										
@@ -60,5 +73,18 @@ describe StoresController do
     		expect(response).to redirect_to subscription_plans_store_url
 		end
 	end #update_promo
+
+	describe 'update_description' do
+		it "WORKING: update description requires admin login and plan_id of 2 or greater" do
+			@store.plan_id = 2
+			@store.save
+			sign_in @admin
+			@new_description = 'this store is awesome'
+			put :update_description, id: @store.id, store: {description: @new_description}
+			expect(response).to redirect_to store_url
+    		@store.reload
+    		expect(@store.description).to eq(@new_description)
+		end
+	end #update_description
 
 end
