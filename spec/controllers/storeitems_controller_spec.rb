406a8f990ad918dd066a95310ba2aef16098a5ee
Daniel Vu <evanidul@gmail.com>
2014-09-10 00:14:43 -0700

    test store item new

--- spec/controllers/storeitems_controller_spec.rb
+++ spec/controllers/storeitems_controller_spec.rb
@@ -46,7 +46,37 @@ describe StoreItemsController do
 			get :index, store_id: @store.id
 			expect(response).to render_template :index			
 		end
-
   	end #index
+
+	describe 'new' do
+  		it 'requires login' do
+			get :new, store_id: @store.id
+			expect(response).to redirect_to new_user_session_url
+		end
+		it 'fails as a normal user' do
+			sign_in @user
+			get :new, store_id: @store.id
+			expect(response).to render_template :error_authorization
+		end
+		it 'fails as another store owner' do
+			@store_other = create(:store)
+			@store_owner_other = create(:user)
+			role_service = Simpleweed::Security::Roleservice.new							
+			role_service.addStoreOwnerRoleToStore(@store_owner_other, @store_other)
+			sign_in @store_owner_other
+
+			get :new, store_id: @store.id
+			expect(response).to render_template :error_authorization			
+		end
+		it 'works as store owner' do
+			role_service = Simpleweed::Security::Roleservice.new							
+			role_service.addStoreOwnerRoleToStore(@user, @store)
+			sign_in @user
+
+			get :new, store_id: @store.id
+			expect(response).to render_template :new			
+		end
+  	end #index
+
 
 end
