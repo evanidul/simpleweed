6ccf174d553832c0e4248b4163440c72f49b21ea
Daniel Vu <evanidul@gmail.com>
2014-09-10 04:02:50 -0700

    test undestroy item

--- spec/controllers/storeitems_controller_spec.rb
+++ spec/controllers/storeitems_controller_spec.rb
@@ -229,6 +229,40 @@ describe StoreItemsController do
 			expect(response).to redirect_to :store_store_items
 			expect(request.flash[:notice]).to eq(@item.name + " has been archived")
 		end
-  	end
+  	end #destroy
+
+  	describe 'undestroy' do
+  		it 'requires login' do
+  			post :undestroy, store_id: @store.id, id: @item.id
+  			expect(response).to redirect_to new_user_session_url
+  		end
+  		it 'fails as a normal user' do
+  			sign_in @user
+  			post :undestroy, store_id: @store.id, id: @item.id
+  			expect(response).to render_template :error_authorization
+  		end
+  		it 'fails as another store owner' do
+			@store_other = create(:store)
+			@store_owner_other = create(:user)
+			role_service = Simpleweed::Security::Roleservice.new							
+			role_service.addStoreOwnerRoleToStore(@store_owner_other, @store_other)
+			sign_in @store_owner_other
+
+			post :undestroy, store_id: @store.id, id: @item.id
+			expect(response).to render_template :error_authorization			
+		end
+		it 'works as store owner' do
+			role_service = Simpleweed::Security::Roleservice.new							
+			role_service.addStoreOwnerRoleToStore(@user, @store)
+			sign_in @user
+
+			# have to destroy an item first...
+			expect{ delete :destroy, store_id: @store.id, id: @item.id }.to change(StoreItem, :count).by(-1)
+												
+			expect{ post :undestroy, store_id: @store.id, id: @item.id}.to change(StoreItem, :count).by(1)
+			expect(response).to redirect_to :store_store_items
+			expect(request.flash[:notice]).to eq(@item.name + " has been unarchived")
+		end
+  	end #undestroy
 
 end
