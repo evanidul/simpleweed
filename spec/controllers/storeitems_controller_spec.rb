abcd1a817145b85f1bee71643c57543cbd96462b
Daniel Vu <evanidul@gmail.com>
2014-09-10 03:40:25 -0700

    test: item edits

--- spec/controllers/storeitems_controller_spec.rb
+++ spec/controllers/storeitems_controller_spec.rb
@@ -150,6 +150,40 @@ describe StoreItemsController do
 		end
   	end #edit
 
-  	
+  	describe 'update' do
+  		it 'requires login' do
+  			new_description = "this is awesome"
+  			patch :update, store_id: @store.id, id: @item.id, store_item: {description: new_description}
+  			expect(response).to redirect_to new_user_session_url
+  		end
+  		it 'fails as a normal user' do
+  			sign_in @user
+  			new_description = "this is awesome"	
+  			patch :update, store_id: @store.id, id: @item.id, store_item: {description: new_description}
+  			expect(response).to render_template :error_authorization
+  		end
+  		it 'fails as another store owner' do
+			@store_other = create(:store)
+			@store_owner_other = create(:user)
+			role_service = Simpleweed::Security::Roleservice.new							
+			role_service.addStoreOwnerRoleToStore(@store_owner_other, @store_other)
+			sign_in @store_owner_other
+
+			new_description = "this is awesome"	
+			patch :update, store_id: @store.id, id: @item.id, store_item: {description: new_description}
+			expect(response).to render_template :error_authorization			
+		end
+  		it 'works as store owner' do
+			role_service = Simpleweed::Security::Roleservice.new							
+			role_service.addStoreOwnerRoleToStore(@user, @store)
+			sign_in @user
+						
+			new_description = "this is awesome"			
+			patch :update, store_id: @store.id, id: @item.id, store_item: {description: new_description}
+			expect(response).to redirect_to :store_store_items
+			@item.reload
+			expect(@item.description).to eq(new_description)			
+		end
+  	end #update
 
 end
