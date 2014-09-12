e68d7491a93ff0e19cadd7c422412d8a9b9e8f25
Daniel Vu <evanidul@gmail.com>
2014-09-12 07:51:16 -0700

    test: flagging requires login

--- spec/features/core/community_management_spec.rb
+++ spec/features/core/community_management_spec.rb
@@ -225,10 +225,108 @@ feature "review a store" , :js => true, :search =>true do
 		header.community_home_link.click
 		community_home_page.dynamic_feed_links.first.click
 		feed_page.post_titles.size.should == 0
+	end
 
+	scenario "login as admin, create a feed, create a post, log out, flag post, login" do	
+		# login as admin
+		page.visit("/")
 
+		header = HeaderPageComponent.new
+		header.has_loginlink?
+		header.loginlink.click
+    	
+    	# login modal
+    	header.username.set @adminemail
+    	header.password.set @adminpassword
+		header.logininbutton.click
 
+		expect(header.edituserlink.text).to have_text(@adminusername)
 
+		# go to community
+		header.community_home_link.click
+
+		community_home_page = CommunityFeedHomePage.new
+		
+		# add new feed
+		community_home_page.add_new_feed_button.click
+
+		new_feed_name = "new stuff"
+		community_home_page.new_feed_name_input.set new_feed_name
+		community_home_page.save_feed_button.click
+
+		community_home_page.dynamic_feed_links.first.click
+
+		feed_page = CommunityFeedPage.new		
+		expect(feed_page.feed_name_span.text).to have_text(new_feed_name)
+
+		# add new post
+		feed_page.add_new_post_button.click
+		post_title = "my first post"
+		post_content = "i don't have much to say"
+		feed_page.feed_post_title_input.set post_title
+		feed_page.feed_post_content_input.set post_content
+		feed_page.save_post_button.click
+		
+		expect(feed_page.post_titles.first.text).to have_text(post_title)
+
+		feed_page.post_upvote_buttons.size.should == 1
+		feed_page.post_upvote_buttons.first.click
+		wait_for_ajax
+		feed_page.post_flash_notices.size.should == 1
+
+		expect(feed_page.post_flash_notices.first.text).to have_text("A user can't vote on their own posts")
+		
+		# log out
+		header.logoutlink.click
+
+		# go to community
+		header.community_home_link.click        
+
+		community_home_page.dynamic_feed_links.first.click
+
+		feed_page.post_flag_links.size.should == 1
+		
+		# flagging should require login
+		feed_page.post_flag_links.first.click
+
+		# login modal
+    	header.username.set @adminemail
+    	header.password.set @adminpassword
+		header.logininbutton.click
+
+		expect(header.edituserlink.text).to have_text(@adminusername)
+		
+		feed_page.post_flag_links.first.click
+		flag_reason = "this is spam"
+		feed_page.flag_reason_input.set flag_reason
+		feed_page.save_flag_button.click
+
+		# flags should appear in admin management			
+		page.visit("/admin/flags/index")
+		page.visit("/admin/flags/index")  # not sure really why, but sometimes the first load doesn't load flags
+		admin_manage_flags_page = AdminManageFlagsPage.new
+		admin_manage_flags_page.delete_flag_buttons.size.should == 1
+		admin_manage_flags_page.delete_flag_buttons.first.click
+		admin_manage_flags_page.delete_flag_buttons.size.should == 0
+		
+
+        # logout
+        header.logoutlink.click
+
+		# go to community
+		header.community_home_link.click        
+		community_home_page.dynamic_feed_links.first.click
+
+		# click on the username of the first post
+		feed_page.post_username_links.first.click
+
+		# must be logged in to see a user profile
+		login_page = LoginPage.new		
+		login_page.has_username_input?		
+		login_page.has_username_password_input?
+		login_page.username_input.set @adminemail
+    	login_page.username_password_input.set @adminpassword        	
+    	login_page.sign_in_button.click		
 	end
 	
 
