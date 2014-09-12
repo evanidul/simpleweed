1e42499a3d323fc79577fcb1c8318a786bfe3996
Daniel Vu <evanidul@gmail.com>
2014-09-11 18:33:07 -0700

    test Item review votes controller

--- spec/controllers/store_item_review_votes_controller_spec.rb
+++ spec/controllers/store_item_review_votes_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe StoreItemReviewVotesController do
+	include Devise::TestHelpers
+
+	before(:each) do
+		user = 'ddadmin'
+		pw = 'idontreallysmoke'
+		request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)    	
+  	end
+
+  	before(:each) do
+  		@store = create(:store)
+  	end
+
+  	before(:each) do
+  		@admin = create(:admin)
+  		@user = create(:user)		  									
+  	end
+
+  	before(:each) do
+  		@item = create(:store_item, :store => @store)
+  	end
+
+  	before(:each) do  		
+  		@item_review = create(:store_item_review, :store_item => @item, :user => @user)
+  	end
+
+  	describe 'create' do
+  		it 'requires login' do
+  			xhr :post, :create, store_id: @store.id, store_item_id: @item.id, store_item_review_id: @item_review.id, store_item_review_vote: attributes_for(:store_item_review_vote)
+  			expect(response).to render_template :login
+  		end  	
+  	end
 
 end
---
+++ spec/factories/store_item_review_votes.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do 
+	factory :store_item_review_vote do		
+		vote "1"		
+	end
+end
