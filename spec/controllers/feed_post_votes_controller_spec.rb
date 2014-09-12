cdfddb009aeba121ba9a51a458446a84d64ab10f
Daniel Vu <evanidul@gmail.com>
2014-09-12 01:57:57 -0700

    test feed post vote controller

--- spec/controllers/feed_post_votes_controller_spec.rb
+++ spec/controllers/feed_post_votes_controller_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe FeedPostVotesController do
+	include Devise::TestHelpers
 
+	before(:each) do
+		user = 'ddadmin'
+		pw = 'idontreallysmoke'
+		request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)    	
+  	end
+
+  	before(:each) do
+  		@feed = create(:feed)
+  	end
+
+  	before(:each) do
+  		@admin = create(:admin)
+  		@user = create(:user)		  									
+  	end
+
+  	before(:each) do
+  		@post = create(:feed_post, :user => @user, :feed => @feed)
+  	end
+
+
+  	describe 'create' do
+  		it 'requires login' do
+  			xhr :post, :create, feed_id: @feed.id, feed_post_id: @post.id, feed_post_vote: attributes_for(:feed_post_vote)			
+			expect(response).to render_template :login
+  		end
+  	end
 end
---
+++ spec/factories/feed_post_votes.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do 
+	factory :feed_post_vote do
+		vote 1
+	end
+end
