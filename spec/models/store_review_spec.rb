24a74ac0fa5f4d8ead6dbc2f9469e65bcd3e3189
Daniel Vu <evanidul@gmail.com>
2014-09-17 07:07:33 -0700

    test for store review model: store owners can't write reviews

--- spec/models/store_review_spec.rb
+++ spec/models/store_review_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe StoreReview do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+		@admin = create(:admin)		
+	end
+
+  	before(:each) do
+  		@store = create(:store)
+  		@store2 = create(:store)
+  	end
+  	
+	it "fails as store owner for his own store" do
+		@store_owner = create(:user)
+		role_service = Simpleweed::Security::Roleservice.new							
+		role_service.addStoreOwnerRoleToStore(@store_owner, @store)
+
+		review = build(:store_review, :user => @store_owner, :store => @store)		
+		expect(review).to have(1).errors_on(:base)
+		review.errors[:base].should include "Store owners and managers cannot write reviews"
+		expect(review).to be_invalid
+	end
+
+	it "fails as store owner for other store" do
+		@store_owner = create(:user)
+		role_service = Simpleweed::Security::Roleservice.new							
+		role_service.addStoreOwnerRoleToStore(@store_owner, @store)
+
+		review = build(:store_review, :user => @store_owner, :store => @store2)		
+		expect(review).to have(1).errors_on(:base)
+		review.errors[:base].should include "Store owners and managers cannot write reviews"
+		expect(review).to be_invalid
+	end
 end
