4cef04f91f082439b9e991866c20693e5b604ea9
Daniel Vu <evanidul@gmail.com>
2014-09-17 02:15:54 -0700

    formatting

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Store do  
 
   it "must have a name" do
-	@store_name = "My new store"
-	@store_addressline1 = "7110 Rock Valley Court"
-	@store_city = "San Diego"
-	@store_ca = "CA"
-	@store_zip = "92122"
-  	@store = Store.new(:name => @store_name , :addressline1 => @store_addressline1, :city => @store_city, :state => @store_ca, :zip => @store_zip)
+  	@store_name = "My new store"
+  	@store_addressline1 = "7110 Rock Valley Court"
+  	@store_city = "San Diego"
+  	@store_ca = "CA"
+  	@store_zip = "92122"
+    @store = Store.new(:name => @store_name , :addressline1 => @store_addressline1, :city => @store_city, :state => @store_ca, :zip => @store_zip)
   	expect(@store).to be_valid
 
   	@failedStore = Store.new()
@@ -18,11 +18,11 @@ describe Store do
 
 
   it "has an address method which will concatenate other fields" do
-	@store_name = "My new store"
-	@store_addressline1 = "7110 Rock Valley Court"
-	@store_city = "San Diego"
-	@store_ca = "CA"
-	@store_zip = "92122"
+  	@store_name = "My new store"
+  	@store_addressline1 = "7110 Rock Valley Court"
+  	@store_city = "San Diego"
+  	@store_ca = "CA"
+  	@store_zip = "92122"
   	@store = Store.new(:name => @store_name , :addressline1 => @store_addressline1, :city => @store_city, :state => @store_ca, :zip => @store_zip)
   	expect(@store).to be_valid
 
