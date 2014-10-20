3b2a11bc7734eab250bf5e103eb5ce1aec3e8923
Daniel Vu <evanidul@gmail.com>
2014-10-19 23:32:49 -0700

    test: verify link text get converted to <a> tags

--- spec/features/extended/community_post_spec.rb
+++ spec/features/extended/community_post_spec.rb
@@ -111,5 +111,56 @@ feature "community extended spec" , :js => true, :search =>true do
 		feed_post.post_post_content.first.find("br")
 	end
 
+	scenario "login as admin, create a feed, create a post with link text, see post and verify that it changed to <a>" do	
+		# login as admin
+		page.visit("/")
+
+		header = HeaderPageComponent.new
+		header.has_loginlink?
+		header.loginlink.click
+    	
+    	# login modal
+    	header.username.set @adminemail
+    	header.password.set @adminpassword
+		header.logininbutton.click
+
+		expect(header.edituserlink.text).to have_text(@adminusername)
+
+		# go to community
+		header.community_home_link.click
+
+		community_home_page = CommunityFeedHomePage.new
+		
+		# add new feed
+		community_home_page.add_new_feed_button.click
+
+		new_feed_name = "new stuff"
+		community_home_page.new_feed_name_input.set new_feed_name
+		community_home_page.save_feed_button.click
+
+		community_home_page.dynamic_feed_links.first.click
+
+		feed_page = CommunityFeedPage.new		
+		expect(feed_page.feed_name_span.text).to have_text(new_feed_name)
+
+		# add new post
+		feed_page.add_new_post_button.click
+		post_title = "my first post"
+		post_content = "http://www.google.com"
+		
+		feed_page.feed_post_title_input.set post_title
+		feed_page.feed_post_content_input.set post_content
+		feed_page.save_post_button.click
+		
+		expect(feed_page.post_titles.first.text).to have_text(post_title)
+
+		# go to post detail
+		feed_page.post_titles.first.click
+		feed_post = CommunityPostPage.new
+		
+		# will return error if <br> hasn't been inserted
+		feed_post.post_post_content.first.find("a")
+	end
+
 	
 end
