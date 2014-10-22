7557e4063aabf0d7741fd6e42ba2f3e8a276a381
Daniel Vu <evanidul@gmail.com>
2014-10-22 03:46:59 -0700

    generate feed slug for new feeds only

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,4 +2,9 @@ class Feed < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :name, use: [:slugged, :finders]
 	has_many :feed_posts
+
+	def should_generate_new_friendly_id?
+    	new_record?
+  	end
+
 end
