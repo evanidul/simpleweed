f45da3d470e696f98d1fd901e5bfbc662183ba23
Daniel Vu <evanidul@gmail.com>
2014-10-22 02:39:29 -0700

    friendly url to feed names

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,3 +1,5 @@
 class Feed < ActiveRecord::Base
+	extend FriendlyId
+	friendly_id :name, use: [:slugged, :finders]
 	has_many :feed_posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141021021623) do
+ActiveRecord::Schema.define(version: 20141022093610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -117,7 +117,10 @@ ActiveRecord::Schema.define(version: 20141021021623) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "slug"
   end
+
+  add_index "feeds", ["slug"], name: "index_feeds_on_slug", using: :btree
 
   create_table "flaggings", force: true do |t|
     t.string   "flaggable_type"
